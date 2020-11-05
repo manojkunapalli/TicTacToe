3f5d60a6668e34f7a7371cd6a36ad3816315bb08
manojkunapalli <manojkunapalli@gmail.com>
2020-11-05 10:36:14 +0530

    Changed:TicTacToeGame class, comments added

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,10 +1,9 @@
 public class TicTacToeGame {
-
+    //UC1 initialize empty board when game is started
     public static char[] startNewGame(){
         char[] board = new char[10];
-
-        for(int i=1; i<board.length;i++) {
-            board[i] = ' ';
+        for(int index=1;index<board.length;index++) {
+            board[index] = ' ';
         }
         return board;
     }
