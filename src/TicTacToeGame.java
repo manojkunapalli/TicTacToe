759b7e1f2b3f0c75aa680b1764c18fd563cb5868
manojkunapalli <manojkunapalli@gmail.com>
2020-11-05 11:40:57 +0530

    Added: Ability to choose letter for User and set letter Computer

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class TicTacToeGame {
     //UC1 initialize empty board when game is started
     public static char[] startNewGame(){
@@ -8,7 +9,24 @@ public class TicTacToeGame {
         return board;
     }
 
+    //UC2 Ability to choose user X or O
+    public static String chooseUserLetter(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("User: Choose X or O");
+        return input.nextLine();
+    }
+
+    //UC2 set the Computer letter
+    public static String setComputerLetter(String userLetter){
+        if(userLetter.equals("O")){
+            return "X";
+        }
+        return "O";
+    }
+
     public static void main(String[] args){
         char[] board = startNewGame();
+        String userLetter = chooseUserLetter();
+        String computerLetter = setComputerLetter(userLetter);
     }
 }
